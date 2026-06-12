8872e80bf0bdee7376d930601418c40b24c9e093
Annaely Oliveira <annaely691@gmail.com>
2026-06-12 11:12:15 -0300

    Mudanças pro pages

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-export default defineConfig(({ command, mode }) => {
+export default defineConfig(({ mode }) => {
   const isProd = mode === 'production' || process.env.NODE_ENV === 'production';
   return {
     base: isProd ? '/Us-valentine-s-day/' : '/',
