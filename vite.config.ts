25292b22c6a94e23f2b4287fb8062efa7606933d
Annaely Oliveira <annaely691@gmail.com>
2026-06-12 11:59:44 -0300

    build: rebuilda para docs com todos os assets

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,9 @@ export default defineConfig(({ mode }) => {
   const isProd = mode === 'production' || process.env.NODE_ENV === 'production';
   return {
     base: isProd ? '/Us-valentine-s-day/' : '/',
+    build: {
+      outDir: 'docs', 
+    },
     plugins: [
       react(),
       tailwindcss(),
